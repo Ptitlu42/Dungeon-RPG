9ce2c1ff2ca2a98e6891101d867f4f2664ec69ee
Antoine Castel <antoine.castel@epsi.fr>
2023-03-24 13:36:54 +0100

    Constructor Item OK

---
+++ Item.py
@@ -0,0 +1,15 @@
+class item:
+    
+    def __init__(self, id, equipable, name, emplacement, strength_mod, life_mod, speed_mod, const_mod, ap_mod, heal) -> None:
+        self.id = id
+        self.equipable = equipable
+        self.name = name
+        self.emplacement = emplacement
+        self.strength_mod = strength_mod
+        self.life_mod = life_mod
+        self.speed_mod = speed_mod
+        self.const_mod = const_mod
+        self.ap_mod = ap_mod
+        self.heal = heal
+
+
