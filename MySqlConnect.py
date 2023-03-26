727c217f293abff10c3f88bcf3dd7ad07788f0d2
Lucas <lucas.beyer@gmx.fr>
2023-03-26 01:20:48 +0100

    bdd print

--- MySqlConnect.py
+++ MySqlConnect.py
@@ -1,15 +1,33 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
+try :
+   mydb = mysql.connector.connect(
   host="localhost",
   user="root",
   password="password",
   database="rpg"
 )
+   print("Connection réussie.")
+except:
+   print("Connection echouée.")
+
 
 cursor = mydb.cursor()
 
+#fetchall sert à print chaque ligne indépendemment et non une seule et même ligne
 cursor.execute("SELECT * FROM item")
+column_names = [description[0] for description in cursor.description]
+print("\n\nListe d'items: \n")
+print(column_names)
+results = cursor.fetchall()
+for row in results:
+    print(row)
 
-for x in cursor:
-  print(x)
+cursor.execute("SELECT * FROM caracter")
+column_names = [description[0] for description in cursor.description]
+print("\n\nListe de caracters: \n")
+print(column_names)
+results = cursor.fetchall()
+for row in results:
+    print(row)
+
--- player.py
+++ player.py
@@ -6,8 +6,9 @@ from Case import Tile
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = pygame.image.load("graphics/sprites/player.png").convert_alpha()
+        self.image = pygame.image.load("sprites/player.png").convert_alpha()
         self.rect = self.image.get_rect(topleft = pos)
         
     # def Run():
     #     pass
+    
