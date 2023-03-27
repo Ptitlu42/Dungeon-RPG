3fc5786bbed67ffe030af9cc80bd3580d4322fd4
Lucas <lucas.beyer@gmx.fr>
2023-03-27 15:47:16 +0200

    rows and cols in the loadmap function

--- map.py
+++ map.py
@@ -7,15 +7,21 @@ actual_map = []
 
 
 # functions
-def load_map(filename, rows, cols):
+def load_map(filename):
     # Open the workbook
     book = xlrd.open_workbook(filename)
+    
+    #Get number of cols and rows
+    worksheet = book.sheet_by_index(0)
+    nbr_cols = worksheet.ncols
+    nbr_rows = worksheet.nrows
+    
     # Get the first sheet
     sheet = book.sheet_by_index(0)
     cell = 0
     # Read the rows and columns
-    for i in range(rows):
-        for j in range(cols):
+    for i in range(nbr_rows):
+        for j in range(nbr_cols):
             # Read the cell value
             cell_value = sheet.cell_value(i, j)
 
@@ -27,3 +33,18 @@ def load_map(filename, rows, cols):
             actual_map.append(case)
             cell += 1
     return actual_map
+
+# def get_number_of_columns_and_rows(rows, cols):
+#     nbr_cols = ()
+#     nbr_rows = ()
+    
+#     for i in range (cols):
+#         nbr_cols + 1
+        
+#     for i in range (rows):
+#         nbr_rows + 1
+        
+#     print (nbr_cols)
+#     print (nbr_rows)
+    
+# get_number_of_columns_and_rows(rows, cols)
