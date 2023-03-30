69aa2f1e7de89046e55c6921cdc891f80f8061e4
Lucas <lucas.beyer@gmx.fr>
2023-03-30 14:25:29 +0200

    chat pnj

--- openaiconnect.py
+++ openaiconnect.py
@@ -10,17 +10,16 @@ GPT_KEY_PTITLU = os.getenv('GPT_KEY_PTITLU')
 
 openai.api_key = GPT_KEY_PTITLU
 
-while True:
-    prompt = input("Prompt:")
-
+def message(promptinput):
     response = openai.Completion.create(
     engine="text-davinci-003",
-    prompt=prompt,
+    prompt=promptinput,
     max_tokens=1000,
     n=1,
     stop=None,
-    temperature=0.5,
+    temperature=0,5,
     )
     
     response = response.choices[0].text.strip()
     print(response)
+    return response
