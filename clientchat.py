3baf3b07e078c3e6ecc9ef5c9ebd0a331afafb11
Lucas <118479475+Ptitlu42@users.noreply.github.com>
2023-03-30 11:15:28 +0200

    chat

---
+++ clientchat.py
@@ -0,0 +1,19 @@
+import socket
+
+hote = "192.168.157.229"
+port = 6666
+
+connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+connexion_avec_serveur.connect((hote, port))
+print(f"Connexion établie avec le serveur sur le port {port}")
+
+msg_a_envoyer = b""
+while msg_a_envoyer != b"fin":
+    msg_a_envoyer = input("> ")
+    msg_a_envoyer = msg_a_envoyer.encode()
+    connexion_avec_serveur.send(msg_a_envoyer)
+    msg_recu = connexion_avec_serveur.recv(1024)
+    print(msg_recu.decode())
+
+print("Fermeture de la connexion")
+connexion_avec_serveur.close()
