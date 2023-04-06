a4193cdfbdba0052e3000d63174b9cc3e301c520
ancastelan <114922837+ancastelan@users.noreply.github.com>
2023-04-06 13:29:57 +0200

    Add files via upload

--- chatclient.py
+++ chatclient.py
@@ -1,33 +1,22 @@
 import socket
-import threading
 
 hote = "192.168.157.229"
 port = 6666
 
-def envoyer_messages(connexion):
-    while True:
-        message = input("> ")
-        message = message.replace('"', '\\"')  # Échapper les guillemets doubles
-        message = message.encode()
-        connexion.send(message)
-
-def recevoir_messages(connexion):
-    while True:
-        message = connexion.recv(1024)
-        print(message.decode())
-
+# Créer un socket pour le client
 connexion_avec_serveur = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 connexion_avec_serveur.connect((hote, port))
 print(f"Connexion établie avec le serveur sur le port {port}")
 
-thread_envoi = threading.Thread(target=envoyer_messages, args=(connexion_avec_serveur,))
-thread_reception = threading.Thread(target=recevoir_messages, args=(connexion_avec_serveur,))
+msg_a_envoyer = ""
+while msg_a_envoyer != "fin":
+    msg_a_envoyer = input("> ")
+    # Envoyer le message au serveur
+    connexion_avec_serveur.send(msg_a_envoyer.encode())
 
-thread_envoi.start()
-thread_reception.start()
-
-thread_envoi.join()
-thread_reception.join()
+    # Recevoir les messages des autres clients
+    messages_recus = connexion_avec_serveur.recv(1024).decode()
+    print(messages_recus)
 
 print("Fermeture de la connexion")
 connexion_avec_serveur.close() 
--- openaiconnect.py
+++ openaiconnect.py
@@ -10,16 +10,17 @@ GPT_KEY_PTITLU = os.getenv('GPT_KEY_PTITLU')
 
 openai.api_key = GPT_KEY_PTITLU
 
-def message(promptinput):
+while True:
+    prompt = input("Prompt:")
+
     response = openai.Completion.create(
     engine="text-davinci-003",
-    prompt=promptinput,
+    prompt=prompt,
     max_tokens=1000,
     n=1,
     stop=None,
-    temperature=0,5,
+    temperature=0.5,
     )
     
     response = response.choices[0].text.strip()
     print(response)
-    return response
