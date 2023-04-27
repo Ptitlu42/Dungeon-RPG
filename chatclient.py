7d3e5256dac508d133439e85e0fff9d19ca644bd
kilianGitB <79051821+KilianGitB@users.noreply.github.com>
2023-04-27 10:55:28 +0200

    test

--- chatclient.py
+++ chatclient.py
@@ -1,4 +1,7 @@
 import socket
+import threading
+
+from chat import envoyer_messages, recevoir_messages
 
 hote = "192.168.1.68"
 port = 6666
@@ -17,6 +20,21 @@ while msg_a_envoyer != "fin":
     # Recevoir les messages des autres clients
     messages_recus = connexion_avec_serveur.recv(1024).decode()
     print(messages_recus)
+
+"""
+creation de 2 threads, un pour l'envoi de message, l'autre pour la reception
+
+"""
+thread_envoi = threading.Thread(target=envoyer_messages, args=(connexion_avec_serveur,))
+thread_reception = threading.Thread(target=recevoir_messages, args=(connexion_avec_serveur,))
+
+#demarrage des threads
+thread_envoi.start()
+thread_reception.start()
+
+#empeche la perte  de messages -> join() -> permet d'attendre que le threads se termine correctement
+thread_envoi.join()
+thread_reception.join()
 
 print("Fermeture de la connexion")
 connexion_avec_serveur.close() 
